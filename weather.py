4ef037f6a2b61d2c37e86fb89b59e616139ab6aa
Egor Gushin <gea11@yandex.ru>
2023-03-21 18:13:20 +0300

    create

--- weather.py
+++ weather.py
@@ -5,5 +5,5 @@ API = 'Your APi key on openweathermap.org'
 
 def weather_cheсk(lat, lon):
     URL = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&APPID={API}&units=metric&lang=ru'
-    response = requests.get(URL.format(lat, lon))
+    response = requests.get(URL)
     return response.json()
